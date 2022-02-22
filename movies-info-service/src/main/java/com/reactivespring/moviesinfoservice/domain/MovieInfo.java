2d8294056cd8d2f9f3c3fbb959a371eb92277378
Mr-randomize <iviberberi@gmail.com>
2022-02-22 18:33:10 +0100

    Commit missing files

--- movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/domain/MovieInfo.java
+++ movies-info-service/src/main/java/com/reactivespring/moviesinfoservice/domain/MovieInfo.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -16,6 +17,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document
+@Validated
 public class MovieInfo {
 
     @Id
@@ -26,6 +28,7 @@ public class MovieInfo {
     @Positive(message = "movieInfo.year must be a positive value")
     private Integer year;
 
+    @NotNull
     private List<@NotBlank(message = "movieInfo.cast must be present") String> cast;
     private LocalDate release_date;
 }
